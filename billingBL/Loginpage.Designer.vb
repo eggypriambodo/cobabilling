65d2de5653abbc22f210a50a54ebd32f8956db90
eggypriambodo <eggypriambodo86@gmail.com>
2024-07-29 23:17:14 +0700

    .

--- billingBL/Loginpage.Designer.vb
+++ billingBL/Loginpage.Designer.vb
@@ -25,91 +25,150 @@ Partial Class Loginpage
         components = New ComponentModel.Container()
         ToolTip1 = New ToolTip(components)
         PictureBox2 = New PictureBox()
-        txtUser = New TextBox()
-        txtPass = New TextBox()
         Label1 = New Label()
-        Label2 = New Label()
-        Button1 = New Button()
+        FlowLayoutPanel1 = New FlowLayoutPanel()
+        PictureBox1 = New PictureBox()
+        txtUser = New TextBox()
+        FlowLayoutPanel2 = New FlowLayoutPanel()
+        PictureBox3 = New PictureBox()
+        txtPass = New TextBox()
+        btnLogin = New Button()
         CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        FlowLayoutPanel1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        FlowLayoutPanel2.SuspendLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' PictureBox2
         ' 
+        PictureBox2.Dock = DockStyle.Top
         PictureBox2.Image = My.Resources.Resources.logobl
-        PictureBox2.Location = New Point(174, 37)
+        PictureBox2.Location = New Point(0, 0)
         PictureBox2.Name = "PictureBox2"
-        PictureBox2.Size = New Size(188, 148)
+        PictureBox2.Size = New Size(537, 148)
         PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox2.TabIndex = 0
         PictureBox2.TabStop = False
         ' 
+        ' Label1
+        ' 
+        Label1.Dock = DockStyle.Top
+        Label1.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Black
+        Label1.Location = New Point(0, 148)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(537, 46)
+        Label1.TabIndex = 1
+        Label1.Text = "Sign In"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' FlowLayoutPanel1
+        ' 
+        FlowLayoutPanel1.Controls.Add(PictureBox1)
+        FlowLayoutPanel1.Controls.Add(txtUser)
+        FlowLayoutPanel1.Location = New Point(118, 211)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        FlowLayoutPanel1.Size = New Size(302, 50)
+        FlowLayoutPanel1.TabIndex = 2
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        PictureBox1.Image = My.Resources.Resources.user
+        PictureBox1.Location = New Point(3, 3)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(63, 43)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
         ' txtUser
         ' 
-        txtUser.Location = New Point(174, 257)
+        txtUser.Dock = DockStyle.Bottom
+        txtUser.Font = New Font("Segoe UI", 12.0F)
+        txtUser.Location = New Point(72, 12)
         txtUser.Name = "txtUser"
-        txtUser.Size = New Size(188, 27)
+        txtUser.Size = New Size(218, 34)
         txtUser.TabIndex = 1
+        txtUser.Text = "Username"
+        ' 
+        ' FlowLayoutPanel2
+        ' 
+        FlowLayoutPanel2.Controls.Add(PictureBox3)
+        FlowLayoutPanel2.Controls.Add(txtPass)
+        FlowLayoutPanel2.Location = New Point(118, 287)
+        FlowLayoutPanel2.Name = "FlowLayoutPanel2"
+        FlowLayoutPanel2.Size = New Size(302, 50)
+        FlowLayoutPanel2.TabIndex = 3
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        PictureBox3.Image = My.Resources.Resources.padlock
+        PictureBox3.Location = New Point(3, 3)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(63, 43)
+        PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox3.TabIndex = 0
+        PictureBox3.TabStop = False
         ' 
         ' txtPass
         ' 
-        txtPass.Location = New Point(174, 326)
+        txtPass.Dock = DockStyle.Bottom
+        txtPass.Font = New Font("Segoe UI", 12.0F)
+        txtPass.Location = New Point(72, 12)
         txtPass.Name = "txtPass"
-        txtPass.Size = New Size(188, 27)
-        txtPass.TabIndex = 2
+        txtPass.Size = New Size(218, 34)
+        txtPass.TabIndex = 1
+        txtPass.Text = "Password"
         ' 
-        ' Label1
+        ' btnLogin
         ' 
-        Label1.AutoSize = True
-        Label1.Location = New Point(174, 234)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(75, 20)
-        Label1.TabIndex = 3
-        Label1.Text = "Username"
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(174, 303)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(70, 20)
-        Label2.TabIndex = 4
-        Label2.Text = "Password"
-        ' 
-        ' Button1
-        ' 
-        Button1.Location = New Point(221, 372)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(94, 29)
-        Button1.TabIndex = 5
-        Button1.Text = "LOGIN"
-        Button1.UseVisualStyleBackColor = True
+        btnLogin.BackColor = Color.DeepSkyBlue
+        btnLogin.Font = New Font("Segoe UI", 12.0F)
+        btnLogin.Location = New Point(118, 350)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(302, 47)
+        btnLogin.TabIndex = 4
+        btnLogin.Text = "Sign In"
+        btnLogin.UseVisualStyleBackColor = False
         ' 
         ' Loginpage
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
         AutoSize = True
+        BackColor = Color.White
         ClientSize = New Size(537, 437)
-        Controls.Add(Button1)
-        Controls.Add(Label2)
         Controls.Add(Label1)
+        Controls.Add(btnLogin)
+        Controls.Add(FlowLayoutPanel1)
+        Controls.Add(FlowLayoutPanel2)
         Controls.Add(PictureBox2)
-        Controls.Add(txtPass)
-        Controls.Add(txtUser)
+        ForeColor = Color.White
         MaximizeBox = False
         Name = "Loginpage"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Login Page"
         CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        FlowLayoutPanel1.ResumeLayout(False)
+        FlowLayoutPanel1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        FlowLayoutPanel2.ResumeLayout(False)
+        FlowLayoutPanel2.PerformLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
-        PerformLayout()
     End Sub
     Friend WithEvents ToolTip1 As ToolTip
     Friend WithEvents PictureBox2 As PictureBox
-    Friend WithEvents txtUser As TextBox
-    Friend WithEvents txtPass As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents FlowLayoutPanel2 As FlowLayoutPanel
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents btnLogin As Button
 
 End Class
--- billingBL/Loginpage.vb
+++ billingBL/Loginpage.vb
@@ -1,26 +1,59 @@
 ﻿Public Class Loginpage
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub txtUser_GotFocus(sender As Object, e As EventArgs) Handles txtUser.GotFocus
+        If txtUser.Text = "Username" Then
+            txtUser.Text = ""
+            txtUser.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub txtUser_LostFocus(sender As Object, e As EventArgs) Handles txtUser.LostFocus
+        If txtUser.Text = "" Then
+            txtUser.Text = "Username"
+            txtUser.ForeColor = Color.DarkGray
+        End If
+    End Sub
+
+    Private Sub txtPass_GotFocus(sender As Object, e As EventArgs) Handles txtPass.GotFocus
+        If txtPass.Text = "Password" Then
+            txtPass.Text = ""
+            txtPass.PasswordChar = "●"
+            txtPass.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub txtPass_lostFocus(sender As Object, e As EventArgs) Handles txtPass.LostFocus
+        If txtPass.Text = "" Then
+            txtPass.Text = "Password"
+            txtPass.PasswordChar = "●"
+            txtPass.ForeColor = Color.DarkGray
+        End If
+    End Sub
+
+    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles FlowLayoutPanel1.Paint
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         Dim username As String
         Dim password As String
         username = txtUser.Text
         password = txtPass.Text
-        If (username.Equals("admin") And password.Equals("admin123")) Then
+        If username.Equals("admin") And password.Equals("admin123") Then
             MessageBox.Show("login success", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
         Else
             MessageBox.Show("username / password is incorrect", "information", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
-
     End Sub
 
-    Private Sub txtUser_TextChanged(sender As Object, e As EventArgs) Handles txtUser.TextChanged
 
-    End Sub
-
-    Private Sub txtPass_TextChanged(sender As Object, e As EventArgs) Handles txtPass.TextChanged
-
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+        If CheckBox1.Checked = True Then
+            txtPass.UseSystemPasswordChar = ""
+        Else
+            txtPass.UseSystemPasswordChar = "●"
+        End If
     End Sub
 End Class
