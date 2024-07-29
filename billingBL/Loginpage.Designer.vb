2484405c37385bdc8568caad21bd322d18f25ba3
eggypriambodo <eggypriambodo86@gmail.com>
2024-07-29 21:27:00 +0700

    Add project files.

---
+++ billingBL/Loginpage.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Loginpage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        ToolTip1 = New ToolTip(components)
+        PictureBox2 = New PictureBox()
+        txtUser = New TextBox()
+        txtPass = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.logobl
+        PictureBox2.Location = New Point(174, 37)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(188, 148)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 0
+        PictureBox2.TabStop = False
+        ' 
+        ' txtUser
+        ' 
+        txtUser.Location = New Point(174, 257)
+        txtUser.Name = "txtUser"
+        txtUser.Size = New Size(188, 27)
+        txtUser.TabIndex = 1
+        ' 
+        ' txtPass
+        ' 
+        txtPass.Location = New Point(174, 326)
+        txtPass.Name = "txtPass"
+        txtPass.Size = New Size(188, 27)
+        txtPass.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(174, 234)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(75, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "Username"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(174, 303)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(70, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "Password"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(221, 372)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 5
+        Button1.Text = "LOGIN"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Loginpage
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
+        ClientSize = New Size(537, 437)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox2)
+        Controls.Add(txtPass)
+        Controls.Add(txtUser)
+        MaximizeBox = False
+        Name = "Loginpage"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Login Page"
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ billingBL/Loginpage.vb
@@ -0,0 +1,26 @@
+﻿Public Class Loginpage
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim username As String
+        Dim password As String
+        username = txtUser.Text
+        password = txtPass.Text
+        If (username.Equals("admin") And password.Equals("admin123")) Then
+            MessageBox.Show("login success", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("username / password is incorrect", "information", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+
+    Private Sub txtUser_TextChanged(sender As Object, e As EventArgs) Handles txtUser.TextChanged
+
+    End Sub
+
+    Private Sub txtPass_TextChanged(sender As Object, e As EventArgs) Handles txtPass.TextChanged
+
+    End Sub
+End Class
